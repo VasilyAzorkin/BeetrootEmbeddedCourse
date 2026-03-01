55a8e716718d3c8a62a13750e35c512b92a4c4d2
VasilyAzorkin <vasily.azorkin@gmail.com>
2026-03-01 13:50:39 +0200

    Finished Relay measuring code

--- HW_22_Relay/src/main.cpp
+++ HW_22_Relay/src/main.cpp
@@ -2,36 +2,58 @@
 
 constexpr uint8_t RELAY_IN_PIN        = 17;
 constexpr uint8_t RELAY_OUT_PIN       = 18;    
-constexpr uint16_t SIGNAL_DELAY_MS    = 2000;
+constexpr uint16_t SIGNAL_DELAY_MS    = 1000;
+constexpr uint8_t DEBOUNCING_DELAY    = 10;
 
-uint64_t prev;
-bool relay_off;
+uint64_t debounce_last = 0;
+uint64_t prev = 0;
+uint16_t count = 0;
+volatile uint32_t total_delay = 0;
+volatile uint16_t curr_delay;
+volatile bool fired = false;
 
 void IRAM_ATTR onRelayInterrupt(){
-  printf("RELAY ON!");
+  
+  if (millis() - debounce_last > DEBOUNCING_DELAY){
+    debounce_last = millis();
+    curr_delay = millis() - prev;
+    fired = true;
+  }
 }
 
 void setup() {
   Serial0.begin(115200);
   pinMode(RELAY_IN_PIN, OUTPUT);
-  pinMode(RELAY_OUT_PIN, INPUT_PULLDOWN);
-  //attachInterrupt(digitalPinToInterrupt(RELAY_OUT_PIN), onRelayInterrupt, RISING);
-  prev = millis();
-  relay_off = true;
-  digitalWrite(RELAY_IN_PIN, relay_off);
+  pinMode(RELAY_OUT_PIN, PULLDOWN);
+  attachInterrupt(digitalPinToInterrupt(RELAY_OUT_PIN), onRelayInterrupt, CHANGE);
 }
 
 void loop() {
-  
-  if(millis() - prev >= SIGNAL_DELAY_MS){
-    relay_off = !relay_off;
-    digitalWrite(RELAY_IN_PIN, relay_off);
-    printf("relay_off: %d\n", relay_off);
-    if (digitalRead(RELAY_OUT_PIN) == HIGH){
-      printf("RELAY ON!");
-    }
-    else 
-      printf("RELAY OFF!");  
-    prev = millis();
+  if (fired){
+    fired = false;
+    total_delay += curr_delay;
+    count++;
+    printf("current:%d\ttotal:%d\tcount:%d\tavg:%d\n", curr_delay, total_delay, count, total_delay/count);
   }
+
+  if(millis() - prev >= SIGNAL_DELAY_MS){
+    const int relay_state = digitalRead(RELAY_OUT_PIN);
+    prev = millis();
+    digitalWrite(RELAY_IN_PIN, relay_state);
+    logRelayState();  
+  }
+}
+
+void logRelayState()
+{
+  String relay_state_text;
+  if (digitalRead(RELAY_OUT_PIN) == HIGH)
+  {
+    relay_state_text = "OFF";
+  }
+  else
+  {
+    relay_state_text = "ON";
+  }
+  printf("\nRelay state: %s\n\n", relay_state_text);
 }
